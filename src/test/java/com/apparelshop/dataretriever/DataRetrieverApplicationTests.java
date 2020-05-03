722c84d4e33c3d0a3ed1377234f78a4865daf47b
Anton Danylov <ur.anton.danilov@gmail.com>
2020-05-03 10:30:20 +0300

    Initial commit

---
+++ src/test/java/com/apparelshop/dataretriever/DataRetrieverApplicationTests.java
@@ -0,0 +1,13 @@
+package com.apparelshop.dataretriever;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DataRetrieverApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
